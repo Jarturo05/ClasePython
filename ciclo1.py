137401ddada1f1c625e299088559ffd078d82464
Jarturo05 <jaecp2702@gmail.com>
2026-03-10 23:43:24 -0500

    Subida de temas vistos en clase Python

---
+++ ciclo1.py
@@ -0,0 +1,18 @@
+
+tabla = int(input("Ingrese la tabla del 1 al 9: "))
+
+i = 1
+
+while i <= 10:
+    print(f"{tabla} * {i} = {i*tabla}")
+    i += 1    
+
+"""# Ciclo while
+
+i = 1
+
+while i <= 10:
+    print(i*7)
+    i += 1
+
+print("Ya terminó el ciclo")"""
---
+++ variables.py
@@ -0,0 +1,11 @@
+edad = 18 
+PI = 3.1416
+nombre = "Juan"
+casado = False
+print(type(edad))
+print(edad)
+print(type(PI))
+print(type(nombre))
+print(type(casado))
+PI = "Pajarito"
+print(type(PI))
